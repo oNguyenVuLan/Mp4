cc06ebb8214d1bd898abd41f693af149d04d7d48
Btnisme123 <vulan238@gmail.com>
2018-02-27 00:37:38 +0700

    create view model

--- app/src/main/java/com/example/vulan/mp4/media/MediaStreaming.kt
+++ app/src/main/java/com/example/vulan/mp4/media/MediaStreaming.kt
@@ -1,36 +1,35 @@
 package com.example.vulan.mp4.media
 
 import android.media.MediaPlayer
+import java.io.IOException
 
 /**
  * Created by vulan on 2/25/2018.
  */
-class MediaStreaming: MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnCompletionListener {
+class MediaStreaming {
     private var mMediaPlayer: MediaPlayer = MediaPlayer()
 
-    public fun init() {
-        mMediaPlayer.setDataSource("http://178.32.116.58:8080/24-6")
-        mMediaPlayer.setOnBufferingUpdateListener(this)
-        mMediaPlayer.setOnCompletionListener(this)
+    fun init() {
+        try {
+            mMediaPlayer = MediaPlayer()
+            mMediaPlayer.setDataSource("http://178.32.116.58:8080/24-6")
+            mMediaPlayer.prepareAsync()
+        } catch (e: IOException) {
+            e.stackTrace
+        }
     }
 
-    public fun play() {
+    fun play() {
         mMediaPlayer.start()
     }
 
-    public fun pause() {
+    fun pause() {
         mMediaPlayer.pause()
     }
 
-    public fun getPlayedState(): Boolean {
+    fun getPlayedState(): Boolean {
         return mMediaPlayer.isPlaying
     }
 
-    override fun onCompletion(p0: MediaPlayer?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
-    override fun onBufferingUpdate(p0: MediaPlayer?, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 }
